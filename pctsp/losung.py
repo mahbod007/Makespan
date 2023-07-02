6c9adfa302df23a08eeffeab8d494994db3aa306
miladnavi <milad.navidizadeh@gmail.com>
2023-07-02 15:39:04 +0200

    multistart optimazation

--- pctsp/losung.py
+++ pctsp/losung.py
@@ -19,6 +19,9 @@ class Node(object):
     def __len__(self):
         return self.length
 
+    def getIndex(self):
+        return self.ind
+    
     def getPrize(self):
         return self.prize
 
@@ -30,6 +33,9 @@ class Node(object):
     
     def node_visited(self):
         self.visited = True
+    
+    def node_not_visited(self):
+        self.visited = False
 
 
 # parsing input file
@@ -79,16 +85,17 @@ while start_node != current_node:
         if not node_instance.isVisted() and current_node != ind:
             if optimal_path < node_instance.getPrize() - cost[current_node][ind] - node_instance.getPenalty():
                 optimal_path = node_instance.getPrize(
-                ) - cost[current_node][ind] - node_instance.getPenalty()
+                ) - cost[current_node][ind]
                 current_node = ind
-    trip_cost += optimal_path
+    trip_cost += optimal_path 
     visited_node_ind.append(current_node)
     node_instances[current_node].node_visited()
 
 # calculate penalty
 for ind, node_instance in enumerate(node_instances):
-    if node_instance.isVisted():
-        trip_cost += node_instance.getPenalty() 
+    if not node_instance.isVisted():
+        trip_cost -= node_instance.getPenalty()
+
 
 print("First Round Trip:")
 print(trip_cost)
@@ -120,4 +127,73 @@ print(trip_cost)
 print(visited_node_ind)    
 
 
+# create nodes instances
+node_instances = []
+for ind, prize in enumerate(prizes):
+    node_instances.append(Node(ind, prize, penalties[ind]))
 
+# create round trip from all nodes
+main_trip_cost = -sys.maxsize
+for main_ind, node_to_strat_with in enumerate(node_instances):
+    for item in node_instances:
+        item.node_not_visited()
+    start_node = main_ind
+    current_node = -1
+    trip_cost = 0
+    visited_node_ind = []
+    visited_node_ind.append(start_node)
+    while start_node != current_node:
+        if current_node == -1:
+            current_node = main_ind
+        optimal_path = -sys.maxsize
+        for ind, node_instance in enumerate(node_instances):
+            if not node_instance.isVisted() and current_node != ind:
+                if optimal_path < node_instance.getPrize() - cost[current_node][ind] - node_instance.getPenalty():
+                    optimal_path = node_instance.getPrize(
+                    ) - cost[current_node][ind] 
+                    current_node = ind
+        trip_cost += optimal_path 
+        visited_node_ind.append(current_node)
+        node_instances[current_node].node_visited()
+
+    # calculate penalty
+    for ind, node_instance in enumerate(node_instances):
+        if not node_instance.isVisted():
+            trip_cost -= node_instance.getPenalty()
+
+
+    temp_visted_node_ind = visited_node_ind
+    temp_trip_cost = trip_cost
+
+    print("Round Trip on node " + str(current_node))
+    print(visited_node_ind)
+
+    for ind in range(1, len(visited_node_ind) - 2):
+        temp_trip_cost += cost[visited_node_ind[ind - 1]][visited_node_ind[ind]]
+        temp_trip_cost += cost[visited_node_ind[ind]][visited_node_ind[ind + 1]]
+        temp_trip_cost += cost[visited_node_ind[ind + 1]
+                            ][visited_node_ind[ind + 2]]
+
+        temp_trip_cost -= cost[visited_node_ind[ind - 1]
+                            ][visited_node_ind[ind + 1]]
+        temp_trip_cost -= cost[visited_node_ind[ind + 1]][visited_node_ind[ind]]
+        temp_trip_cost -= cost[visited_node_ind[ind]][visited_node_ind[ind + 2]]
+
+        if temp_trip_cost > trip_cost:
+            trip_cost = temp_trip_cost
+            temp_swap = temp_visted_node_ind[ind]
+            temp_visted_node_ind[ind] = temp_visted_node_ind[ind + 1]
+            temp_visted_node_ind[ind + 1] = temp_swap
+            visited_node_ind = temp_visted_node_ind
+        else:
+            temp_trip_cost = trip_cost
+
+    print("Optimized Round Trip on node " + str(current_node))
+    print(visited_node_ind)
+    
+    if trip_cost > main_trip_cost:
+        main_trip_cost = trip_cost
+
+print("Overall Optimization on all Nodes:")
+print(main_trip_cost)
+print(visited_node_ind)
